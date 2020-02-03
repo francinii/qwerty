c738c6085f9436cdad83337aa0580507ecb7926e
ajcg1995 <38873916+ajcg1995@users.noreply.github.com>
2020-02-03 09:43:20 -0600

    Commit cambios

--- includes.php
+++ includes.php
@@ -3,6 +3,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
 <link href="inc/css/main.css" rel="stylesheet" type="text/css"/>
+<link href="inc/css/lineaTiempo.css" rel="stylesheet" type="text/css"/>
+
 
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta name="descripcion" content="Qwerty Costa Rica"/>	
--- template/home.php
+++ template/home.php
@@ -134,12 +134,94 @@
             <img src="img/layout/frame2.png">
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 section_benefits">
-            hola
+
+
+            <div class="timeline">
+                <div class="container left">
+                    <div class="content">
+                        <h1>2017</h1>
+                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                        <h1>2016</h1>
+                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                    </div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                        <h1>2015</h1>
+                        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                    </div>
+                </div>                
+            </div>
         </div>
+    </div>
+
+    <div class="row section_idear">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-md-offset-2 contendorCirculos">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-2  ">
+                    <div class="circuloDos">1</div>
+                    <div class="informacion-circulo">
+                        <p>Esto es informacion de lekjlqk daf</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-2  ">
+                    <div class="circuloDos"></div>
+                    <div class="informacion-circulo">
+                        <p>Esto es informacion de lekjlqk daf</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-2  ">
+                    <div class="circuloDos"></div>
+                    <div class="informacion-circulo">
+                        <p>Esto es informacion de lekjlqk daf</p>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-2  ">
+                    <div class="circuloDos"></div>
+                    <div class="informacion-circulo">
+                        <p>Esto es informacion de lekjlqk daf</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 
     <div class="row section_portafolio">
 
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1">
+                <h2> Nuestro <br>
+                    portafolio </h2>
+            </div>
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1">
+                <img src="img/content/cliente2">
+            </div>
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1">
+                <img src="img/content/cliente2">
+            </div>
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1">
+                <img src="img/content/cliente2">
+            </div>
+
+            <div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1">
+                <img src="img/content/cliente2">
+            </div>
+
+        </div>
+
     </div>
+
+
 
 </div>
